2f5e7b652f4eb8e9be62ad56fda0b337b7064e58
smartcuedevops <smartcuedevops@gmail.com>
2019-05-07 23:26:35 +0530

    fixing task2 NOV-2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String args[])
     {
-        System.out.println("integrating jira with git!");
+        System.out.println("integrating jira with git! and updating for task 2");
     }
 }
